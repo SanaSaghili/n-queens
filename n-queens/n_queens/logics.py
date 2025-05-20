c870121f5890da7e98ac2365681e9c14ac935d65
Sana Saghili <s.saghili@gmail.com>
2025-05-20 10:14:30 -0700

    Describe the changes you made to logics.py

--- n-queens/n_queens/logics.py
+++ n-queens/n_queens/logics.py
@@ -1,21 +1,41 @@
+# وارد کردن کلاس‌های مربوط به الگوریتم‌های حل مسئله n-Queens
 from algorithms.backtracking_algorithm import BacktrackingAlgorithm
 from algorithms.genetic_algorithm import GeneticAlgorithm
 
+# سازنده‌ی کلاس NQueensController
+#n: اندازه‌ی صفحه شطرنج و تعداد وزیرها
 class NQueensController:
+
     def __init__(self, n):
         self.n = n
 
     def set_n(self, n):
+        """
+        تنظیم مقدار جدید برای n
+        """
         self.n = n
 
+    """
+    اجرای الگوریتم دقیق Backtracking
+    خروجی: لیستی از جواب‌های معتبر (هر جواب یک لیست از موقعیت وزیرها در ردیف‌های مختلف است)
+    """
     def run_backtracking_algorithm(self):
+
         solver = BacktrackingAlgorithm(self.n)
         return solver.all_solutions()
 
+    """
+    اجرای الگوریتم ژنتیکی (تقریبی)
+    خروجی: لیستی از جواب‌های پیدا شده با تکامل ژنتیکی
+    """
     def run_genetic_algorithm(self):
+
         genetic = GeneticAlgorithm(self.n)
         return genetic.run_evolution()
 
+
+    #تابع رزرو شده برای الگوریتم CSP (هنوز پیاده‌سازی نشده)
     def run_csp_algorithm(self):
-        pass
-        return true
+
+        pass  
+        return True
