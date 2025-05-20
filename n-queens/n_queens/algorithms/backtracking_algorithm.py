9a6d488cf3054b242e82f07d0107e285f28d269b
Sana Saghili <s.saghili@gmail.com>
2025-05-20 10:04:31 -0700

    Add comments to backtracking_algorithm.py

--- n-queens/n_queens/algorithms/backtracking_algorithm.py
+++ n-queens/n_queens/algorithms/backtracking_algorithm.py
@@ -1,27 +1,39 @@
+# کد مربوط به الگوریتم عقبگرد
 class BacktrackingAlgorithm:
+
+    # مقداردهی اولیه الگوریتم عقبگرد
+    # n:تعداد وزیر
     def __init__(self, n):
-        self.size = n
-        self.find_solution = []
+
+        self.size = n  # اندازه صفحه
+        self.find_solution = []  # لیست جواب‌های پیدا شده
 
     def check(self, rows, columns, temp):
+        #بررسی شروط تعارض
         for row in range(rows):
+            # بررسی تعارض ستونی یا قطری
             if temp[row] == columns or abs(temp[row] - columns) == abs(row - rows):
-                return False
-        return True
+                return False  # تعارض وجود دارد
+        return True  # بدون تعارض
 
+#تابع بازگشتی برای پیدا کردن تمام راه‌حل‌ها با استفاده از عقبگرد
     def find(self, row, temp):
+
         if row == self.size:
+            # اگر همه ردیف‌ها با موفقیت پر شدند، یک راه‌حل معتبر پیدا شده است
             self.find_solution.append(temp[:])
             return
+        
+        # برای هر ستون در ردیف فعلی امتحان می‌کنیم
         for c in range(self.size):
-            if self.check(row, c, temp):
-                temp[row] = c
-                self.find(row + 1, temp)
-                temp[row] = -1
+            if self.check(row, c, temp):  # اگر قرار دادن وزیر در (row, c) معتبر است
+                temp[row] = c  # وزیر را قرار بده
+                self.find(row + 1, temp)  # برو به ردیف بعد
+                temp[row] = -1  # backtrack (برگرداندن به حالت قبلی)
 
+    #اجرای الگوریتم و برگشت دادن تمام جواب‌های معتبر
     def all_solutions(self):
+
         temp = [-1] * self.size
         self.find(0, temp)
         return self.find_solution
-
-
